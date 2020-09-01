06dbc3f92a423bd1865abca1ad8c8f0550d995af
megazol <50046183+megazol3000@users.noreply.github.com>
2020-09-02 00:32:11 +0500

    Add files via upload

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,10 +3,13 @@ const searchInput = document.querySelector(".search-input");
 const burgerBtn = document.querySelector(".header-burger");
 const body = document.querySelector("body");
 const footer = document.querySelector("footer");
-const menu = document.querySelector(".mobile-menu");
-const social = document.querySelector(".menu-social");
-const aboutus = document.querySelector(".about-us");
-const submenu = document.querySelector(".sub-menu");
+const menu = document.querySelector('.mobile-menu');
+const social = document.querySelector('.menu-social');
+const aboutus = document.querySelector('.about-us');
+const submenu = document.querySelector('.sub-menu');
+const whatArrow = document.querySelectorAll('.what_answer_title');
+const answer = document.querySelectorAll('.what_answer');
+const answerArrow = document.querySelectorAll('.what_answer__arrow');
 
 searchBtn.addEventListener('click', function showInput() {
 	searchBtn.classList.toggle('search-button--active');
@@ -30,4 +33,13 @@ for(i=0; i<arrow.length; i++){
 	subMenu.classList.toggle('open');
 	thisArrow.classList.toggle('submenu-active');
 });
+}
+
+for(j=0; j<answer.length; j++){
+	let thisAnswer = answer[j];
+	let thisAnswerArrow = answerArrow[j];
+	thisAnswer.addEventListener('click', function(){
+		thisAnswer.classList.toggle('closed');
+		thisAnswerArrow.classList.toggle('rotate');
+	});
 }
